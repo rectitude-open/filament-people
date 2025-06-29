312a955560ede4e96a9405777f81aae6acc87a49
aspirantzhang <admin@aspirantzhang.com>
2025-06-29 16:28:49 +0800

    feat: implement getModel method to return configured model class

--- src/FilamentPeople.php
+++ src/FilamentPeople.php
@@ -4,4 +4,16 @@ declare(strict_types=1);
 
 namespace RectitudeOpen\FilamentPeople;
 
-class FilamentPeople {}
+use Illuminate\Database\Eloquent\Model;
+use RectitudeOpen\FilamentPeople\Models\People;
+
+class FilamentPeople
+{
+    /**
+     * @return class-string<Model>
+     */
+    public function getModel(): string
+    {
+        return config('filament-people.people.model', People::class);
+    }
+}
